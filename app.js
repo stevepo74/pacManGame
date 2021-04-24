c0be9ac8a4e64616fff868607dd8ff651326041e
Steve P. Olson <36646456+stevepo74@users.noreply.github.com>
2021-04-24 08:57:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -63,5 +63,48 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  createBoard();
+  createBoard()
+
+  //starting position of Pac-man
+  let pacmanCurrentIndex = 490
+
+  squares[pacmanCurrentIndex].classList.add('pac-man')
+
+  //move pac-man. e stands for event
+  function movePacman(e) {
+
+    //remove pacman from the squares you're currently in. if you're starting
+    //at the beginning of the game, the current square you're in is the pacman
+    //current index (490)
+    squares[pacmanCurrentIndex].classList.remove('pac-man')
+
+    switch(e.keyCode) {
+      //keycode for left arrow key is 37
+      case 37:
+        if(pacmanCurrentIndex % width != 0) pacmanCurrentIndex -= 1
+        break
+      //38 is keycode for up arrow. logic is pacman is allowed to keep moving up
+      case 38:
+        if(pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -= width
+      //39 is right arrow
+      case 39:
+        if(pacmanCurrentIndex % width < width - 1) pacmanCurrentIndex += 1
+        break
+      //40 is down arrow
+      case 40:
+        if(pacmanCurrentIndex + width < width * width) pacmanCurrentIndex += width
+        break
+    }
+
+    squares[pacmanCurrentIndex].classList.add('pac-man')
+
+    //pacDotEaten()
+    //powerPelletEaten()
+    //checkForGameOver()
+    //checkForWin()
+  
+
+  }
+
+
 });
